55da74da520b6218be4656d65736587011d3d41f
Qutada Samaha <qsamaha98@gmail.com>
2020-06-09 22:28:30 -0400

    First Commit

---
+++ src/com/example/quickMaths/Main.java
@@ -0,0 +1,14 @@
+package com.example.quickMaths;
+
+import java.util.Scanner;
+
+public class Main {
+
+    private Scanner x = new Scanner(System.in);
+    public static void main(String[] args) {
+
+        X x = new X();
+
+        x.x(1);
+    }
+}
---
+++ src/com/example/quickMaths/X.java
@@ -0,0 +1,10 @@
+package com.example.quickMaths;
+
+public class X {
+
+    public void x(int x){
+        for(int X = 0; X <= 12; X++){
+            System.out.println(X + ": " + x * X);
+        }
+    }
+}
